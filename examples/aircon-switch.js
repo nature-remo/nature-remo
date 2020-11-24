3eaa3745e24722ac491d46b8c278f6c3cd8287bf
Yasuaki Uechi <y@uechi.io>
2020-11-24 10:30:20 +0900

    docs: fix typo

--- examples/aircon-switch.js
+++ examples/aircon-switch.js
@@ -5,23 +5,23 @@ async function toggleAirconSwitch() {
   // Instantiate Nature Remo client with an API token
   const client = new NatureRemo.Cloud(process.env.NATURE_REMO_CLOUD_API_TOKEN)
 
-  // Get a list of available air conditioners
+  // Get a list of air conditioners
   const airconList = await client.listAircon()
 
-  // Use an air conditioner in the first of the list
+  // Pick an air conditioner from the top of the list
   const aircon = airconList[0]
   console.log(aircon)
 
-  // Check if air conditioner is powered on
-  const isPoweredOn = aircon.settings.button === 'power-off' ? false : true
+  // Check if the air conditioner is running
+  const isPoweredOn = aircon.settings.button !== 'power-off'
   console.log(isPoweredOn)
 
-  // Toggle air conditioner's switch
+  // Toggle the air conditioner's switch
   if (isPoweredOn) {
-    // Turn off aircon
+    // Turn off the aircon
     await client.updateAirconSettings(aircon.id, { button: 'power-off' })
   } else {
-    // Turn on aircon with specific operation mode
+    // Turn on the aircon with specific operation mode
     await client.updateAirconSettings(aircon.id, {
       operation_mode: aircon.settings.mode,
     })
